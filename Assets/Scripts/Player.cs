67a2652bb39090b458a81e0bba7546cefcac14b4
Michael Henderson <michael.henderson@ubisoft.com>
2020-09-04 15:12:02 -0500

    intial add

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private CharacterController _controller;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //get horizontal input
+        //define direction based on that input
+        //move based on that direction
+    }
+}
